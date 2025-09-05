7b560cb35a89c9d1b7eea95d8f3e0efd0fc9683f
[Caleb Cha] <junoocha@gmail.com>
2025-09-04 23:59:29 -0400

    comment for clarity

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,4 +1,4 @@
-// app/admin/layout.tsx
+// app/(admin)/admin/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
--- src/app/(users)/layout.tsx
+++ src/app/(users)/layout.tsx
@@ -1,3 +1,4 @@
+//app/(users)/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
